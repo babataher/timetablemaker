a57cbe61ce10cf83a038ecd7cd7f28cfee15d6d6
Maysam Torabi <kherad@gmail.com>
2017-11-28 01:08:00 +0330

    show contact info in index for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def welcome
-    render json: {hello: :world}
+    render json: {
+      owner: 'maysam torabi',
+      email: 'kheradATgmailDOTcom'
+    }
   end
 end
